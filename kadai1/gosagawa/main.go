6b62ab140877d60419e424398a892164ecd28cd7
GoSagawa <drunkturtle@gmail.com>
2018-08-17 17:16:16 +0900

    convert image

--- kadai1/gosagawa/main.go
+++ kadai1/gosagawa/main.go
@@ -4,9 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"image"
-	_ "image/gif"
-	_ "image/jpeg"
-	_ "image/png"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -15,21 +15,30 @@ import (
 func main() {
 
 	var (
-		inputImageType  = flag.String("i", "jpeg", "変換対象の画像形式(jpeg|gif|png)")
-		outputImageType = flag.String("o", "png", "変換語の画像形式(jpeg|gif|png)")
+		inType  = flag.String("i", "jpeg", "変換対象の画像形式(jpeg|gif|png)")
+		outType = flag.String("o", "png", "変換語の画像形式(jpeg|gif|png)")
+		dispLog = flag.Bool("v", false, "詳細なログを表示")
 	)
 	flag.Parse()
 	args := flag.Args()
 
-	fmt.Println(*inputImageType)
-	fmt.Println(*outputImageType)
+	fmt.Println(*inType)
+	fmt.Println(*outType)
 	fmt.Println(args[0])
 
 	//TODO check parameter
 
 	dir := args[0]
-	convertList := getConvertList(*inputImageType, dir)
-	fmt.Println(convertList)
+	convertPaths := getConvertList(*inType, dir)
+	for _, path := range convertPaths {
+		dist := getConvertToPath(*outType, path)
+
+		//TODO check file is already exist
+		if *dispLog {
+			fmt.Printf("%v => %v\n", path, dist)
+		}
+		convertImage(*outType, path, dist)
+	}
 }
 
 func getConvertList(imageType string, dir string) []string {
@@ -65,4 +74,40 @@ func getImageType(path string) string {
 		return ""
 	}
 	return format
+}
+
+func getConvertToPath(outType string, path string) string {
+	return path[:len(path)-len(filepath.Ext(path))] + "." + outType
+}
+
+func convertImage(outType string, src string, dest string) {
+	f, err := os.Open(src)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	out, err := os.Create(dest)
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	switch outType {
+	case "jpeg":
+		//TODO control by option
+		opts := &jpeg.Options{}
+		jpeg.Encode(out, img, opts)
+	case "png":
+		png.Encode(out, img)
+	case "gif":
+		//TODO control by option
+		opts := &gif.Options{}
+		gif.Encode(out, img, opts)
+	}
 }
