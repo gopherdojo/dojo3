7bfff8b4a408ecae8737cd1bb649de0fbe04eccb
asuforce <owata.sn@gmail.com>
2018-08-18 00:01:20 +0900

    Move to main function

--- kadai1/asuforce/converter/converter.go
+++ kadai1/asuforce/converter/converter.go
@@ -8,77 +8,68 @@ import (
 	"regexp"
 )
 
-type converter struct {
-	path  string
-	files []image
+// Converter struct
+type Converter struct {
+	Path  string
+	Files []Image
 }
 
-type image struct {
+// Image information struct
+type Image struct {
 	path string
 	name string
 	ext  string
 }
 
 // Convert image functon
-func Convert(path string) error {
-	var c converter
+func Convert(i Image) error {
+	file, err := os.Open(i.path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-	c.path = path
-	err := filepath.Walk(c.path, c.crawlFile)
+	img, err := jpeg.Decode(file)
 	if err != nil {
 		return err
 	}
 
-	for _, v := range c.files {
-		file, err := os.Open(v.path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
+	outputFile, err := os.Create(i.getFileName())
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
 
-		img, err := jpeg.Decode(file)
-		if err != nil {
-			return err
-		}
-
-		outputFile, err := os.Create(v.getFileName())
-		if err != nil {
-			return err
-		}
-		defer outputFile.Close()
-
-		err = png.Encode(outputFile, img)
-		if err != nil {
-			return err
-		}
+	err = png.Encode(outputFile, img)
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func (c *converter) crawlFile(path string, info os.FileInfo, err error) error {
-
+func (c *Converter) CrawlFile(path string, info os.FileInfo, err error) error {
 	if filepath.Ext(path) == ".jpg" {
 		if !info.IsDir() {
 			var i = newImage(path)
-			c.files = append(c.files, i)
+			c.Files = append(c.Files, i)
 		}
 	}
 	return nil
 }
 
-func newImage(path string) image {
+func newImage(path string) Image {
 	ext := filepath.Ext(path)
 	rep := regexp.MustCompile(ext + "$")
 	name := filepath.Base(rep.ReplaceAllString(path, ""))
 
-	return image{
+	return Image{
 		path: path,
 		name: name,
 		ext:  ext,
 	}
 }
 
-func (i *image) getFileName() string {
+func (i *Image) getFileName() string {
 	return i.name + ".png"
 }
--- kadai1/asuforce/main.go
+++ kadai1/asuforce/main.go
@@ -4,21 +4,45 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/gopherdojo/dojo3/kadai1/asuforce/converter"
 )
 
-var path string
+var (
+	version string
+	path    string
+)
 
 func init() {
+	var showVersion bool
+	flag.BoolVar(&showVersion, "v", false, "Show version")
+	flag.BoolVar(&showVersion, "version", false, "Show version")
 	flag.Parse()
+
+	if showVersion {
+		fmt.Println("Version: ", version)
+		os.Exit(0)
+	}
+
 	path = flag.Arg(0)
 }
 
 func main() {
-	err := converter.Convert(path)
+	var c converter.Converter
+
+	c.Path = path
+	err := filepath.Walk(c.Path, c.CrawlFile)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+
+	for _, image := range c.Files {
+		err := converter.Convert(image)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 }
