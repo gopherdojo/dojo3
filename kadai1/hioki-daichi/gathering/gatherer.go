263ab9e3873443e87e1abb03ac6eea49614c3c0d
Daichi Hioki <hiokidaichi@gmail.com>
2018-08-19 13:13:27 +0900

    Extract function: WalkFn

--- kadai1/hioki-daichi/gathering/gatherer.go
+++ kadai1/hioki-daichi/gathering/gatherer.go
@@ -18,33 +18,35 @@ type Gatherer struct {
 
 // Gather searches under the specified directory and collects files to be decoded.
 func (g *Gatherer) Gather(dirname string) ([]string, error) {
-	err := filepath.Walk(dirname, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		if !g.Decoder.HasProcessableExtname(path) {
-			return nil
-		}
-
-		fp, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer fp.Close()
-
-		if !g.Decoder.IsDecodable(fp) {
-			return nil
-		}
-
-		g.Pathnames = append(g.Pathnames, path)
-
-		return nil
-	})
+	err := filepath.Walk(dirname, g.walkFn)
 
 	return g.Pathnames, err
+}
+
+func (g *Gatherer) walkFn(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		return nil
+	}
+
+	if !g.Decoder.HasProcessableExtname(path) {
+		return nil
+	}
+
+	fp, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+
+	if !g.Decoder.IsDecodable(fp) {
+		return nil
+	}
+
+	g.Pathnames = append(g.Pathnames, path)
+
+	return nil
 }
