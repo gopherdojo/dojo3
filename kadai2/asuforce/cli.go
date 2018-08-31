1ea7c1933b00cadcd0db6cc4059e57b84f0bc160
asuforce <owata.sn@gmail.com>
2018-08-31 13:39:18 +0900

    Create cli.go

---
+++ kadai2/asuforce/cli.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/gopherdojo/dojo3/kadai2/asuforce/converter"
+)
+
+// Exit codes are int values.
+const (
+	ExitCodeOK    int = 0
+	ExitCodeError int = 1 + iota
+)
+
+// CLI is the command line object
+type CLI struct {
+	outStraem, errStream io.Writer
+}
+
+// Run invokes the CLI
+func (cli *CLI) Run(args []string) int {
+	var (
+		version     = "1.0.0"
+		showVersion bool
+		path        string
+		fromExt     string
+		toExt       string
+		wg          sync.WaitGroup
+	)
+
+	flag.StringVar(&fromExt, "f", "jpg", "Specify input image extension")
+	flag.StringVar(&fromExt, "from", "jpg", "Specify input image extension")
+	flag.StringVar(&toExt, "t", "png", "Specify output image extension")
+	flag.StringVar(&toExt, "to", "png", "Specify output image extension")
+
+	flag.BoolVar(&showVersion, "v", false, "Show version")
+	flag.BoolVar(&showVersion, "version", false, "Show version")
+
+	flag.Parse()
+
+	if showVersion {
+		fmt.Fprintf(cli.errStream, "Version: %s\n", version)
+		return ExitCodeOK
+	}
+
+	path = flag.Arg(0)
+
+	c, err := converter.NewConverter(path, fromExt, toExt)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return ExitCodeError
+	}
+
+	err = filepath.Walk(c.Path, c.CrawlFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return ExitCodeError
+	}
+
+	queue := make(chan converter.Image)
+	for _, image := range c.Files {
+		wg.Add(1)
+		go c.FetchConverter(queue, &wg)
+		queue <- image
+	}
+
+	close(queue)
+	wg.Wait()
+
+	return ExitCodeOK
+}
--- kadai2/asuforce/main.go
+++ kadai2/asuforce/main.go
@@ -1,61 +1,10 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"os"
-	"path/filepath"
-	"sync"
-
-	"github.com/gopherdojo/dojo3/kadai2/asuforce/converter"
 )
-
-var (
-	version = "1.0.0"
-	path    string
-	fromExt string
-	toExt   string
-	wg      sync.WaitGroup
-)
-
-func init() {
-	var showVersion bool
-	flag.BoolVar(&showVersion, "v", false, "Show version")
-	flag.BoolVar(&showVersion, "version", false, "Show version")
-	flag.StringVar(&fromExt, "f", "jpg", "Specify input image extension")
-	flag.StringVar(&fromExt, "from", "jpg", "Specify input image extension")
-	flag.StringVar(&toExt, "t", "png", "Specify output image extension")
-	flag.StringVar(&toExt, "to", "png", "Specify output image extension")
-	flag.Parse()
-
-	if showVersion {
-		fmt.Println("Version: ", version)
-		os.Exit(0)
-	}
-
-	path = flag.Arg(0)
-}
 
 func main() {
-	c, err := converter.NewConverter(path, fromExt, toExt)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	err = filepath.Walk(c.Path, c.CrawlFile)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	queue := make(chan converter.Image)
-	for _, image := range c.Files {
-		wg.Add(1)
-		go c.FetchConverter(queue, &wg)
-		queue <- image
-	}
-
-	close(queue)
-	wg.Wait()
+	cli := &CLI{outStraem: os.Stdout, errStream: os.Stderr}
+	os.Exit(cli.Run(os.Args))
 }
