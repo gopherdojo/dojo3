e68e4b1533949c6d5a0124f5e0f13b1e1cb05c28
Go Takagi <takagi@shimastripe.com>
2018-09-03 03:01:39 +0900

    Update test

--- kadai1/shimastripe/imgconv/imgconv_test.go
+++ kadai1/shimastripe/imgconv/imgconv_test.go
@@ -5,17 +5,19 @@ import (
 	"image"
 	"image/gif"
 	"image/jpeg"
+	"image/png"
 	"testing"
+)
+
+var (
+	rect = image.Rect(0, 0, 1, 1)
+	img  = image.NewGray(rect)
 )
 
 func TestConvertJpgToGif(t *testing.T) {
 	output := new(bytes.Buffer)
-	rect := image.Rect(0, 0, 1, 1)
-	img := image.NewGray(rect)
-
 	jpgBuf := new(bytes.Buffer)
 	jpeg.Encode(jpgBuf, img, nil)
-
 	gifBuf := new(bytes.Buffer)
 	gif.Encode(gifBuf, img, nil)
 
@@ -24,5 +26,20 @@ func TestConvertJpgToGif(t *testing.T) {
 
 	if !bytes.Equal(output.Bytes(), gifBuf.Bytes()) {
 		t.Errorf("Output=%v, want=%v", output.Bytes(), gifBuf.Bytes())
+	}
+}
+
+func TestConvertJpgToPng(t *testing.T) {
+	output := new(bytes.Buffer)
+	jpgBuf := new(bytes.Buffer)
+	jpeg.Encode(jpgBuf, img, nil)
+	pngBuf := new(bytes.Buffer)
+	png.Encode(pngBuf, img)
+
+	imgConverter := ImageConverter{}
+	imgConverter.Convert(jpgBuf, output, "png")
+
+	if !bytes.Equal(output.Bytes(), pngBuf.Bytes()) {
+		t.Errorf("Output=%v, want=%v", output.Bytes(), pngBuf.Bytes())
 	}
 }
