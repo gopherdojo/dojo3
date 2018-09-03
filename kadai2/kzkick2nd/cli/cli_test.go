caf4dd4b863635cb1ef3fff4b47fd91f5a8a0791
kzkick2nd <aruga.kazuki@gmail.com>
2018-09-03 17:29:57 +0900

    Add cli test

---
+++ kadai2/kzkick2nd/cli/cli_test.go
@@ -0,0 +1,43 @@
+package cli
+
+import (
+  "testing"
+)
+
+// FIXME フォルダ構造のassert追加
+
+func TestRunWithArgs(t *testing.T) {
+
+  patterns := []struct {
+		name string
+		inputExt string
+		outputExt string
+		dir string
+	}{
+		{
+      name: "jpg to png with dir",
+      inputExt: "jpg",
+      outputExt: "png",
+      dir: "../convert/testfiles/",
+    },
+    {
+      name: "jpg to png with 1 file",
+      inputExt: "jpg",
+      outputExt: "png",
+      dir: "../convert/testfiles/1px.jpg",
+    },
+	}
+  for _, p := range patterns {
+    t.Run(p.name, func(t *testing.T) {
+      w := &Worker{
+        Input:  p.inputExt,
+    	  Output: p.outputExt,
+      }
+      err := w.Run(p.dir)
+    	if err != nil {
+        t.Errorf("p.name: %v", err)
+    	}
+    })
+  }
+
+}
