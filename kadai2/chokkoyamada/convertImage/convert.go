9467cebf35239556947782f1ea9706e7a669226b
chokkoyamada <yamada@kirishikistudios.com>
2018-09-03 16:31:36 +0900

    refactor for tests

--- kadai2/chokkoyamada/convertImage/convert.go
+++ kadai2/chokkoyamada/convertImage/convert.go
@@ -1,18 +1,13 @@
-/*
-	Change image format.
-	Available format are png, jpeg, gif.
- */
+/* Change image format. Available format are png, jpeg, gif. */
 package convertImage
 
 import (
-	"os"
-	"fmt"
-	"net/http"
-	"path/filepath"
 	"image"
 	"image/jpeg"
 	"image/png"
 	"image/gif"
+	"io"
+			"bytes"
 )
 
 //Abstracted image
@@ -21,62 +16,25 @@ type AbstractImage struct {
 }
 
 //encode image according to the destination type
-func(ai AbstractImage) encode(out *os.File, destType *string) {
+func (ai AbstractImage) encode(destType *string)(buf bytes.Buffer) {
 	switch *destType {
 	case "png":
-		png.Encode(out, ai.in)
+		png.Encode(&buf, ai.in)
 	case "jpeg":
-		jpeg.Encode(out, ai.in, nil)
+		jpeg.Encode(&buf, ai.in, nil)
 	case "gif":
-		gif.Encode(out, ai.in, nil)
+		gif.Encode(&buf, ai.in, nil)
 	}
+	return buf
 }
 
 //convert image file from one to another
-func Convert(targetDir *string, src string, srcType *string, destType *string) {
-	file, err := os.Open(src)
+func Convert(in io.Reader, destType *string)(b []byte) {
+	img, _, err := image.Decode(in)
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-
-	fileType, err := getFileContentType(file)
-	if err != nil {
-		panic(err)
-	}
-
-	if fileType != "image/"+*srcType {
-		fmt.Println(src + ": image format is not " + *srcType)
-		return
-	}
-
-	file.Seek(0, 0)
-	img, _, err := image.Decode(file)
-	if err != nil {
-		panic(err)
-	}
-	ai := AbstractImage{in:img}
-
-	out, err := os.Create(*targetDir + getFileNameWithoutExt(src) + "." + *destType)
-	if err != nil {
-		panic(err)
-	}
-	defer out.Close()
-
-	ai.encode(out, destType)
+	ai := AbstractImage{in: img}
+	return ai.encode(destType).Bytes()
 }
 
-func getFileContentType(out *os.File) (string, error) {
-	buffer := make([]byte, 512)
-	_, err := out.Read(buffer)
-	if err != nil {
-		return "", err
-	}
-
-	contentType := http.DetectContentType(buffer)
-	return contentType, nil
-}
-
-func getFileNameWithoutExt(path string) string {
-	return filepath.Base(path[:len(path)-len(filepath.Ext(path))])
-}
--- kadai2/chokkoyamada/main.go
+++ kadai2/chokkoyamada/main.go
@@ -6,14 +6,17 @@ import (
 	"log"
 	"path/filepath"
 	"./convertImage"
-	"bytes"
+	"fmt"
+	"github.com/pkg/errors"
+	"io"
+	"net/http"
 )
 
 func main() {
 	var (
 		targetDir = flag.String("targetDir", "./", "対象ディレクトリ")
-		srcType = flag.String("srcType", "jpeg", "変換元ファイルタイプ")
-		destType = flag.String("destType", "png", "変換先ファイルタイプ")
+		srcType   = flag.String("srcType", "jpeg", "変換元ファイルタイプ")
+		destType  = flag.String("destType", "png", "変換先ファイルタイプ")
 	)
 	flag.Parse()
 
@@ -22,8 +25,32 @@ func main() {
 
 func walkFile(targetDir *string, srcType *string, destType *string) {
 	err := filepath.Walk(*targetDir, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir(){
-			convertImage.Convert(targetDir, path, srcType, destType)
+		if !info.IsDir() {
+			inputFile, err := os.Open(path)
+			if err != nil {
+				panic(err)
+			}
+			defer inputFile.Close()
+
+			fileType, err := getFileContentType(inputFile)
+			if err != nil {
+				panic(err)
+			}
+
+			if fileType != "image/"+*srcType {
+				fmt.Println(": image format is not " + *srcType)
+				return errors.New("image format is not " + *srcType)
+			}
+
+			outputFile, err := os.Create(*targetDir + getFileNameWithoutExt(path) + "." + *destType)
+			if err != nil {
+				panic(err)
+			}
+			defer outputFile.Close()
+
+			inputFile.Seek(0, 0)
+			b := convertImage.Convert(inputFile, destType)
+			outputFile.Write(b)
 		}
 		return nil
 	})
@@ -32,12 +59,17 @@ func walkFile(targetDir *string, srcType *string, destType *string) {
 	}
 }
 
-func writeFile() {
-	file, err := os.Create("hoge.txt")
-	if err != nil {
-		panic(err)
-	}
-	file.Write([]byte("fuga"))
-	bytes.Buffer.
+func getFileNameWithoutExt(path string) string {
+	return filepath.Base(path[:len(path)-len(filepath.Ext(path))])
 }
 
+func getFileContentType(out io.Reader) (string, error) {
+	buffer := make([]byte, 512)
+	_, err := out.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	contentType := http.DetectContentType(buffer)
+	return contentType, nil
+}
