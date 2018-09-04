a440a207ab1d8a1d2c9e4b8a8569be73791a813d
asuforce <owata.sn@gmail.com>
2018-09-04 13:28:29 +0900

    Create basic code
    This is buggy

--- kadai3-1/asuforce/main.go
+++ kadai3-1/asuforce/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	words := []string{"hoge", "fuga", "piyo"}
+	stdin := bufio.NewScanner(os.Stdin)
+	score := 0
+
+	rand.Seed(time.Now().UnixNano())
+
+	for {
+		theme := words[rand.Intn(3)]
+		fmt.Println("Type it:", theme)
+
+		for stdin.Scan() {
+			answer := stdin.Text()
+			if theme == answer {
+				score++
+				fmt.Println("ok")
+			} else {
+				fmt.Printf("got: %v, want: %v\n", answer, theme)
+			}
+			fmt.Println("Your score:", score)
+
+			break
+		}
+	}
 }
