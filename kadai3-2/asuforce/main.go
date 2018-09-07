8757504a430a0534178733eb7a3006ba91be9362
asuforce <owata.sn@gmail.com>
2018-09-07 18:46:32 +0900

    Add downloading function

--- kadai3-2/asuforce/main.go
+++ kadai3-2/asuforce/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	url := "https://dummyimage.com/600x400.jpg"
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	fmt.Println("status:", res.Status)
+
+	_, filename := path.Split(url)
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if err := file.Close(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	io.Copy(file, res.Body)
 }
