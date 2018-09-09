eedde0cab874dc7ffde44aca9adceef0a1a56fb1
matsumatsu20 <matsumatsu20@gmail.com>
2018-09-10 01:24:29 +0900

    Change limit time

--- kadai3-1/matsumatsu20/main.go
+++ kadai3-1/matsumatsu20/main.go
@@ -17,7 +17,7 @@ func main() {
 	prefectures := []string{"hokkaido", "aomori", "iwate", "miyagi", "akita", "yamagata", "fukushima", "ibaraki", "tochigi", "gunma", "saitama", "chiba", "tokyo", "kanagawa", "niigata", "toyama", "ishikawa", "fukui", "yamanashi", "nagano", "gifu", "shizuoka", "aichi", "mie", "shiga", "kyoto", "osaka", "hyogo", "nara", "wakayama", "tottori", "shimane", "okayama", "hiroshima", "yamaguchi", "tokushima", "kagawa", "ehime", "kochi", "fukuoka", "saga", "nagasaki", "kumamoto", "oita", "miyazaki", "kagoshima", "okinawa"}
 
 	go func() {
-		<-time.After(3 * time.Second)
+		<-time.After(30 * time.Second)
 
 		fmt.Println("終了しました")
 		fmt.Printf("あなたのスコア: %v\n", count)
