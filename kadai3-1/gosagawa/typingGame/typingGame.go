f9382aa0b9a6b27fa106f61c2dd7b168f97f02fb
GoSagawa <drunkturtle@gmail.com>
2018-09-09 23:23:39 +0900

    complete as typing game

--- kadai3-1/gosagawa/typingGame/typingGame.go
+++ kadai3-1/gosagawa/typingGame/typingGame.go
@@ -1,29 +1,89 @@
 package typingGame
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"time"
+)
 
-func Start() {
-	fmt.Println("start")
+var questions []string
 
-	word := chooseWord()
-	displayWord(word)
-	input := inputWord()
-	isCorrect := check(word, input)
-	displayResult(isCorrect)
+const playTime int = 10
+
+func init() {
+	questions = getQuestions()
 }
 
+func Start() {
+	var num int
+	var endGame bool
+	inputCh := input(os.Stdin)
+
+	startDisplay()
+	<-inputCh
+
+	timeCh := time.After(time.Duration(playTime) * time.Second)
+
+	for {
+		word := chooseWord()
+		displayWord(word)
+
+		select {
+		case input := <-inputCh:
+			isCorrect := check(word, input)
+			displayResult(isCorrect)
+			if isCorrect {
+				num++
+			}
+
+		case <-timeCh:
+			endDisplay(num)
+			endGame = true
+		}
+
+		if endGame {
+			break
+		}
+	}
+}
+
+func getQuestions() []string {
+	var List = []string{"white", "yellow", "orange", "red", "pink", "purple", "blue", "green", "brown", "grey", "black"}
+	return List
+}
+
+func input(r io.Reader) <-chan string {
+	ch := make(chan string)
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- s.Text()
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func startDisplay() {
+	fmt.Println("Please type same words as much as possible ...")
+	fmt.Println("Hit any key to start..")
+	fmt.Println("")
+
+}
 func chooseWord() string {
+	rand.Seed(time.Now().UnixNano())
+	q := questions[rand.Intn(len(questions))]
+	return q
+
 	return "test"
 }
 
 func displayWord(word string) {
 	fmt.Println(word)
-}
-
-func inputWord() string {
-	var stdin string
-	fmt.Scan(&stdin)
-	return stdin
+	fmt.Printf(">")
 }
 
 func check(word string, input string) bool {
@@ -39,4 +99,11 @@ func displayResult(isCorrect bool) {
 	} else {
 		fmt.Println("Miss...")
 	}
+	fmt.Println("")
+}
+
+func endDisplay(correctNumber int) {
+	fmt.Println("")
+	fmt.Println("Time's up !!!")
+	fmt.Printf("Your score is %v\n", correctNumber)
 }
